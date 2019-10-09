f4345db817d8f71cef015906791f7e60d9aac2a2
ii02735 <muhammad.yadallee1@etu.parisdescartes.fr>
2019-10-09 22:11:11 +0200

    console + example of file writing

---
+++ first.js
@@ -0,0 +1,9 @@
+console.log("hello world");
+
+//Example for writing a text file : import needed though
+
+const fs = require('fs'); //fs for File System
+//fs.writeFileSync("file.txt","hello world !!"); //create file.txt and write inside it
+fs.writeFileSync("file.txt","hello new version of nodejs !");
+
+

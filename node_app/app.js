8bf1ab4d76c68972dfbe5c16e4d14357d09e9365
ii02735 <muhammad.yadallee1@etu.parisdescartes.fr>
2019-10-12 16:02:11 +0200

    Example of HTML response

--- node_app/app.js
+++ node_app/app.js
@@ -6,9 +6,22 @@ const http = require('http'); //because we don't intend to overwrite the package
 //this is an example of event writing in node :
 // If a HTTP request comes, we handle it by executing the function inside createServer()
 const server = http.createServer((req,res)=>{ //req = Request, incoming request; res = Response, content sent to client
-    console.log(req); 
+    console.log(req.url, req.method); 
     //process.exit(); //by writing that, we unregister a listener (listen()) after handling a request
     //Because this is the UNIQUE listener, unregister this we cause the NODE application to FINISH
+    res.setHeader('Content-Type','text/html');
+    res.write(`
+    <html>
+    <head>
+        <title>Hello world</title>
+    </head>
+    <body>
+        <h1>It works</h1>
+    </body>
+    </html>
+    `); //Example of sending a response
+    res.end(); //After this line, writing more code will result to an error. That line means that NODEJS must send the response to the client
+    
 })
 
 
@@ -31,5 +44,5 @@ console.log("Listening to 8001 port..."); //will still display because listen is
  * and because a NODE server is a SINGLE THREADED one !
  */
 
-//because we haven't written HTML content yet, accessing to server will stuck, and req object will be displayed to console
+//because we haven't written HTML content yet, accessing to server will stuck (proof of event loop), and req object will be displayed to console
 

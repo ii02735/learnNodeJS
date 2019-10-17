db46a10a1e8ca1f662493363bce9043ee2715922
ik04346 <muhammad.yadallee1@etu.parisdescartes.fr>
2019-10-17 08:47:36 +0200

    Add of handlebars and ejs

--- expressjs/dynamicViews/app.js
+++ expressjs/dynamicViews/app.js
@@ -4,9 +4,16 @@ const rootDir = require("./util/rootDir");
 const path = require("path");
 const routes = require("./routes/routes");
 
+const expressHbs = require("express-handlebars");
+                                    //instead of pug, we define layouts to be inherited in variables
+app.engine("handlebars",expressHbs({defaultLayout: "", layoutsDir: ""})); //to include handlebars template engine, (instead of use set() first)
 //before using a template engine, we must add one to express (much simplier) and not to NODE
 //set() : modifying configuration of expressJS
-app.set("view engine","pug"); //enabling pug as view engine
+//app.set("view engine","pug"); //enabling pug as view engine
+app.set("view engine", "handlebars"); //enabling handlebars as view engine
+
+app.set("view engine", "ejs"); //enabling ejs as view engine
+
 app.set("views","views"); //telling where expressJS needs to find templates to compile
 app.use(express.static(path.join(__dirname,"public")));
 app.use(routes);
--- expressjs/dynamicViews/routes/routes.js
+++ expressjs/dynamicViews/routes/routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const path = require("path")
 router.get("/route",(req,res,next)=>{
     //If we want to render a template engine file instead of a HTML one, 
     //we must use render()
@@ -19,6 +19,49 @@ router.get("/route",(req,res,next)=>{
         }
     ]
     res.render("showBooks.pug", {dynamicValue: req.query.user, books: books}); //its already knows to look into "views" because we told him into app.js
+})
+
+router.get("/hbs",(req,res,next)=>{
+    const value = 1000;
+    res.render("template.handlebars", { variable: "hello world", value: value, isGreatherThan20: value > 20, 
+        books: [
+            {
+                author: "John Doe",
+                title: "The red feather"
+            },
+            {
+                author: "John Doe",
+                title: "The fugitive's chronicles"
+            },
+            {
+                author: "Richard Sean",
+                title: "The sunk boat"
+            }
+        ]}
+    )
+})
+
+router.get("/ejs",(req,res,next)=>{
+    const value = 1000;
+    res.render("template.ejs", { variable: "hello world", value: 1000, 
+        books: [
+            {
+                author: "John Doe",
+                title: "The red feather"
+            },
+            {
+                author: "John Doe",
+                title: "The fugitive's chronicles"
+            },
+            {
+                author: "Richard Sean",
+                title: "The sunk boat"
+            }
+        ]}
+    )
+})
+router.get("/form",(req,res,next)=>{
+    res.sendFile(path.join(__dirname,"..","form.html"));
 })
 
 module.exports = router;
